d5d78910a470560122a9407335a573af26f444bf
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2023-09-16 18:31:16 +0530

    pushing frontend to the github

---
+++ src/components/Login/Login.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router";
+import "./Login.css";
+
+const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    const baseUrl = "http://localhost:5000";
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const user = { email, password };
+
+        axios({
+            method: "POST",
+            url: `${baseUrl}/user/login`,
+            headers: {
+                "Content-Type": "application/json",
+            },
+            data: user,
+        })
+            .then((res) => {
+                console.log("User logged In...");
+                const token = res.data.token;
+                window.localStorage.setItem("token", token);
+                navigate("/dashboard")
+            })
+            .catch((error) => {
+                alert("Authenticaton failed!");
+                setEmail("");
+                setPassword("");
+            });
+    };
+
+    const handleSignUp = (e) => {
+        navigate("/signup")
+    };
+
+    return (
+        <div className="Login">
+            <h1>Login</h1>
+            <form className="LoginForm">
+                <input
+                    type="email"
+                    className="login-FormInput"
+                    required
+                    value={email}
+                    onChange={(e) => {
+                        setEmail(e.target.value);
+                    }}
+                    placeholder="Enter Email"
+                />
+                <input
+                    type="password"
+                    className="login-FormInput"
+                    required
+                    value={password}
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                    placeholder="Enter Password"
+                />
+                <button className="login-btn" onClick={handleLogin}>Login</button>
+                <p className="login-para">
+                    Don't have an account?{" "}
+                    <span onClick={handleSignUp}> SignUp </span>
+                </p>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router";
+import "./SignUp.css";
+
+const SignUp = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    const baseUrl = "http://localhost:5000";
+
+    const handleSignUp = (e) => {
+        e.preventDefault();
+        const user = { name, email, password };
+
+        axios({
+            method: "POST",
+            url: `${baseUrl}/user/register`,
+            headers: {
+                "Content-Type": "application/json",
+            },
+            data: user,
+        })
+            .then((res) => {
+                console.log("New User Created...");
+                window.localStorage.setItem("token",res.data.token);
+                navigate("/dashboard")
+            })
+            .catch((error) => {
+                alert(error);
+                setName("");
+                setEmail("");
+                setPassword("");
+            });
+    };
+
+    const handleLogin = (e) => {
+      navigate("/")
+    };
+
+    return (
+        <div className="Signup">
+            <h1>SignUp</h1>
+            <form className="SignUpForm">
+                <input
+                    type="text"
+                    className="Signup-FormInput"
+                    required
+                    value={name}
+                    onChange={(e) => {
+                        setName(e.target.value);
+                    }}
+                    placeholder="Enter Name"
+                />
+                <input
+                    type="email"
+                    className="Signup-FormInput"
+                    required
+                    value={email}
+                    onChange={(e) => {
+                        setEmail(e.target.value);
+                    }}
+                    placeholder="Enter Email"
+                />
+                <input
+                    type="password"
+                    className="Signup-FormInput"
+                    required
+                    value={password}
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                    placeholder="Enter Password"
+                />
+                <button onClick={handleSignUp}>Login</button>
+                <p>
+                    Already Have an account ?{" "}
+                    <span onClick={handleLogin}> Log In </span>
+                </p>
+            </form>
+        </div>
+    );
+};
+
+export default SignUp;
